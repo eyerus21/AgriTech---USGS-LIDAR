ea66dc7b683808e3837f93626289b92367a59a86
eyerus21 <eyugher21@gmail.com>
2021-08-20 01:35:12 +0300

    update in python scripts

--- scripts/sampler.py
+++ scripts/sampler.py
@@ -1,56 +1,105 @@
-import logging
+import numpy as np
+import laspy as lp
+import sys
+import logger_creator
+
+logger = logger_creator.CreateLogger('CloudSubSampler')
+logger = logger.get_default_logger()
 
 
-class CreateLogger:
-    def __init__(self, name: str, handlers: int = 1, file_name: str = 'log.log') -> None:
-        # Create a custom logger
-        self.logger = logging.getLogger(name)
-        self.num_handlers = handlers
-        self.file_name = file_name
+class CloudSubSampler():
+    def __init__(self, file_name: str):
+        self.point_cloud = self.read_point_cloud_file(file_name)
+        self.available_samplings = ['factor', 'barycenter', 'closest']
+        logger.info('Successfully Instantiated CloudSubSampler Class Object')
 
-    def create_handlers(self) -> None:
-        if(self.num_handlers == 1):
-            # Create Only StreamHandler for console logging
-            self.create_console_handler()
-            self.cons_handler.setFormatter(self.create_handle_format(0))
-            self.add_handlers([self.cons_handler])
+    def read_point_cloud_file(self, file_name: str):
+        try:
+            clouds = lp.read(file_name)
 
-        elif(self.num_handlers == 2):
-            # Create Only FileHandler for file logging
-            self.create_file_handler(self.file_name)
-            self.file_handler.setFormatter(self.create_handle_format(1))
-            self.add_handlers([self.file_handler])
+            logger.info(f'Successfully Loaded point cloud form {file_name}')
+
+            return clouds
+
+        except Exception as e:
+            logger.exception('Failed to load point clouds from File')
+            sys.exit(1)
+
+    def separate_points_colors(self):
+        try:
+            if(not hasattr(self, 'points') and not hasattr(self, 'colors')):
+                self.points = np.vstack(
+                    (self.point_cloud.x, self.point_cloud.y, self.point_cloud.z)).transpose()
+
+                self.colors = np.vstack((self.point_cloud.red, self.point_cloud.green,
+                                        self.point_cloud.blue)).transpose()
+
+            else:
+                logger.info('Points already Calculated, using previous values')
+
+        except Exception as e:
+            logger.exception(
+                'Failed to separate points and colors from point cloud')
+            sys.exit(1)
+
+    def get_factor_subsampling(self, factor: int):
+        try:
+            self.separate_points_colors()
+            if(not hasattr(self, 'factored_points') and not hasattr(self, 'factored_colors')):
+                self.factored_points = self.points[::factor]
+                self.factored_colors = self.colors[::factor]
+
+                return self.factored_points, self.factored_colors
+            else:
+                logger.info(
+                    'Factoring Already Done Previously, using previous values')
+
+        except:
+            logger.exception(
+                'Failed to sample cloud points using factoring')
+            sys.exit(1)
+
+    def get_grid_subsampling(self, voxel_size: int, sampling_type: str = 'closest'):
+        self.separate_points_colors()
+        if(sampling_type != 'closest' or sampling_type != 'barycenter_sample'):
+            raise
+        elif(sampling_type == 'closest' and not hasattr(self, 'candidate_center') or sampling_type == 'barycenter_sample' and not hasattr(self, 'barycenter_sample')):
+            nb_vox = np.ceil((np.max(self.points, axis=0) -
+                             np.min(self.points, axis=0)) / voxel_size)
+
+            non_empty_voxel_keys, inverse, nb_pts_per_voxel = np.unique(
+                ((self.points - np.min(self.points, axis=0)) // voxel_size).astype(int), axis=0, return_inverse=True, return_counts=True)
+            idx_pts_vox_sorted = np.argsort(inverse)
+            voxel_grid = {}
+            grid_barycenter, grid_candidate_center = [], []
+            last_seen = 0
+
+            for idx, vox in enumerate(non_empty_voxel_keys):
+                voxel_grid[tuple(
+                    vox)] = self.points[idx_pts_vox_sorted[last_seen:last_seen+nb_pts_per_voxel[idx]]]
+                grid_barycenter.append(np.mean(voxel_grid[tuple(vox)], axis=0))
+                grid_candidate_center.append(voxel_grid[tuple(vox)][np.linalg.norm(
+                    voxel_grid[tuple(vox)]-np.mean(voxel_grid[tuple(vox)], axis=0), axis=1).argmin()])
+
+                last_seen += nb_pts_per_voxel[idx]
+
+            self.barycenter_sample = grid_barycenter
+            self.candidate_center = grid_candidate_center
+
+            return self.candidate_center if sampling_type == 'closest' else self.barycenter_sample
+
         else:
-            # Create Both Handlers
-            self.create_console_handler()
-            self.create_file_handler(self.file_name)
-            self.cons_handler.setFormatter(self.create_handle_format(0))
-            self.file_handler.setFormatter(self.create_handle_format(1))
-            self.add_handlers([self.cons_handler, self.file_handler])
+            print('already calcuated')
+            return self.candidate_center if sampling_type == 'closest' else self.barycenter_sample
 
-    def create_console_handler(self) -> None:
-        self.cons_handler = logging.StreamHandler()
-        self.cons_handler.setLevel(logging.DEBUG)
+    def save_cloud(self, filename: str, sampling_type: str):
+        """Save the variable to an ASCII file to open in a 3D Software"""
+        if(sampling_type == 'barycenter'):
+            sample_data = self.barycenter_sample
+        elif(sampling_type == 'closest'):
+            sample_data = self.candidate_center
+        elif(sampling_type == 'factor'):
+            sample_data = self.factored_points
 
-    def create_file_handler(self, file) -> None:
-        self.file_handler = logging.FileHandler(filename=file)
-        self.file_handler.setLevel(logging.ERROR)
-
-    def create_handle_format(self, type: int = 0) -> logging.Formatter:
-        if(type == 0):
-            return logging.Formatter('%(name)s:%(levelname)s->%(message)s')
-        elif(type == 1):
-            return logging.Formatter('%(asctime)s:%(name)s:%(levelname)s->%(message)s')
-        else:
-            return logging.Formatter('%(levelname)s->%(message)s')
-
-    def add_handlers(self, handle_list: list) -> None:
-        for handle in handle_list:
-            self.logger.addHandler(handle)
-
-    def get_default_logger(self) -> logging.Logger:
-        # Run create_handlers
-        self.create_handlers()
-        self.logger.setLevel(logging.INFO)
-
-        return self.logger
+        np.savetxt(filename+f'_{sampling_type}'+"_sampled.xyz",
+                   sample_data, delimiter=";", fmt="%s")
